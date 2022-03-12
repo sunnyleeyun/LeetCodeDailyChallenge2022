dee419f48b5042dca2b9698cac8a96630ee8e85a
sunnyleeyun <sunnylee945@hotmail.com>
2022-03-12 23:55:01 +0800

    Add day 80 - 138. Copy List with Random Pointer

---
+++ d067-138CopyListwithRandomPointer.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return head;
+        Node *curr = head;
+        // duplicate node
+        while (curr) {
+            Node *node = new Node(curr->val);
+            node->next = curr->next;
+            curr->next = node;
+            curr = curr->next->next;
+        }
+        curr = head;
+        // setup random
+        while (curr) {
+            if (curr->random) curr->next->random = curr->random->next;
+            curr = curr->next->next;
+        }
+        Node *cpy = head->next;
+        curr = head;
+        // split origin and cpy
+        while (curr) {
+            Node *next = curr->next;
+            curr->next = next->next;
+            if (next->next) next->next = next->next->next;
+            curr = curr->next;
+        }
+        return cpy;
+    }
+};
